61f4223fcbc9dd408e36dbb3d6cc9fe7f883263d
Toms Janson <mcomsis@gmail.com>
2013-01-04 18:31:57 +0000

    Revert "anytypeToStr() changes"
    This reverts commit 685047a113dfcbbbc9ae454fae318f1c2b7499dd.

--- dbop/dbop.go
+++ dbop/dbop.go
@@ -34,29 +34,11 @@ func toStmtStr(value string, valueType string) string {
 
 func anytypeToStr(value interface{}) string {
 	switch value.(type) {
-		case int:
-			return strconv.FormatInt(value.(int),10)
-		case int8:
-			return strconv.FormatInt(value.(int8),10)
-		case int16:
-			return strconv.FormatInt(value.(int16),10)
-		case int32:
-			return strconv.FormatInt(value.(int32),10)
-		case int64:
-			return strconv.FormatInt(value.(int64),10)
-		case uint:
-			return strconv.FormatUint(value.(uint), 10)
-		case uint8:
-			return strconv.FormatUint(value.(uint8), 10)
-		case uint16:
-			return strconv.FormatUint(value.(uint16), 10)
-		case uint32:
-			return strconv.FormatUint(value.(uint32), 10)
-		case uint64:
+		case int, int8, int16, int32, int64:
+			return strconv.FormatInt(value.(int64),10) // TODO change this
+		case uint, uint8, uint16, uint32, uint64:
 			return strconv.FormatUint(value.(uint64), 10)
-		case float32:
-			return strconv.FormatFloat(value.(float32), 'f', -1, 64)
-		case float64:
+		case float32, float64:
 			return strconv.FormatFloat(value.(float64), 'f', -1, 64)
 		case bool:
 			return strconv.FormatBool(value.(bool))
